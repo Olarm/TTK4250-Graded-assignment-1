f3b9806043d4fb8b46500eba29195319529934bd
Ola Rasmus Moen <ola.moen@wellstarter.com>
2019-11-01 11:43:45 +0100

    quat2rotmat implementation

--- GradedEx2/quat2rotmat.m
+++ GradedEx2/quat2rotmat.m
@@ -1,7 +1,10 @@
 function R = quat2rotmat(quat)  
     eta = quat(1);
-    e = quat(2:4);
+    e = quat(2:4);          % n
     S = crossProdMat(e);
     
-    R = %
+    I = eye(3);
+    
+    % cos(a)I + (1 - cos(a))nn' + sin(a)S(n)
+    R = cos(eta)*I + (1 - cos(eta))*(e*e') + sin(eta)*S;
 end
